fd24115d2fedd21ac0b14fdb6b996373a5e45562
Will Leyton <wleyton@addi.com>
2020-01-25 16:28:30 -0500

    Decoders

--- src/main/scala/com/leysoft/products/adapter/in/api/Codecs.scala
+++ src/main/scala/com/leysoft/products/adapter/in/api/Codecs.scala
@@ -1,6 +1,6 @@
 package com.leysoft.products.adapter.in.api
 
-import io.circe.{Decoder, Encoder}
+import io.circe.{Decoder, Encoder, Json}
 import com.leysoft.products.domain
 
 object Codecs {
@@ -12,8 +12,21 @@ object Codecs {
   private val STOCK = "stock"
 
   implicit val productDecoder: Decoder[domain.Product] =
-    Decoder.forProduct2(NAME, STOCK)(domain.Product.apply)
+    // Decoder.forProduct2(NAME, STOCK)(domain.Product.apply)
+    Decoder.instance[domain.Product] { cursor =>
+      for {
+        name <- cursor.downField(NAME).as[String]
+        stock <- cursor.downField(STOCK).as[Double]
+      } yield domain.Product(name, stock)
+    }
 
   implicit val productEncoder: Encoder[domain.Product] =
-    Encoder.forProduct3(ID, NAME, STOCK)(p => (p.id, p.name, p.stock))
+    // Encoder.forProduct3(ID, NAME, STOCK)(p => (p.id, p.name, p.stock))
+    Encoder.instance[domain.Product] { p =>
+      Json.obj(
+        (ID, Json.fromString(p.id)),
+        (NAME, Json.fromString(p.name)),
+        (STOCK, Json.fromDoubleOrString(p.stock))
+      )
+    }
 }
