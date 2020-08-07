b71f93920d1113bf1d847fda4038145c982ab0c0
Will Leyton <wleylop22@gmail.com>
2020-08-07 16:43:43 -0500

    Update

--- src/main/scala/com/leysoft/products/adapter/out/doobie/DoobieProductRepository.scala
+++ src/main/scala/com/leysoft/products/adapter/out/doobie/DoobieProductRepository.scala
@@ -3,45 +3,53 @@ package com.leysoft.products.adapter.out.doobie
 import cats.effect.Effect
 import com.leysoft.products.adapter.out.doobie.util.Doobie
 import com.leysoft.products.domain.{Product, ProductRepository}
+import doobie.util.query.Query0
+import doobie.util.update.Update0
 
 final class DoobieProductRepository[P[_]: Effect: Doobie] private (
 ) extends ProductRepository[P] {
-  import doobie.implicits._
+  import DoobieProductRepository._
 
   override def findBy(id: String): P[Option[Product]] =
-    Doobie[P]
-      .option(sql"SELECT * FROM products WHERE id = $id".query[Product])
+    Doobie[P].option(getById(id))
 
   override def findAll: P[List[Product]] =
-    Doobie[P]
-      .list(sql"SELECT * FROM products".query[Product])
+    Doobie[P].list(getAll)
 
   override def findAllAStreams: fs2.Stream[P, Product] =
-    Doobie[P]
-      .stream(sql"SELECT * FROM products".query[Product])
+    Doobie[P].stream(getAll)
 
   override def save(product: Product): P[Int] =
-    Doobie[P]
-      .command(
-        sql"""INSERT INTO products
-             |VALUES(${product.id}, ${product.name}, ${product.stock})""".stripMargin.update
-      )
+    Doobie[P].command(insert(product))
 
   override def update(product: Product): P[Int] =
-    Doobie[P]
-      .command(
-        sql"""UPDATE products
-             |SET name = ${product.name}, stock = ${product.stock}
-             |WHERE id = ${product.id}""".stripMargin.update
-      )
+    Doobie[P].command(updated(product))
 
   override def delete(id: String): P[Int] =
-    Doobie[P]
-      .command(sql"DELETE FROM products WHERE id = $id".update)
+    Doobie[P].command(deleted(id))
 }
 
 object DoobieProductRepository {
+  import doobie.implicits._
 
   def make[P[_]: Effect: Doobie]: P[DoobieProductRepository[P]] =
     Effect[P].delay(new DoobieProductRepository[P])
+
+  private def getById(id: String): Query0[Product] =
+    sql"SELECT * FROM products WHERE id = $id".query[Product]
+
+  private def getAll: Query0[Product] =
+    sql"SELECT * FROM products".query[Product]
+
+  private def insert(product: Product): Update0 =
+    sql"""INSERT INTO products
+         |VALUES (${product.id}, ${product.name}, ${product.stock})""".stripMargin.update
+
+  private def updated(product: Product): Update0 =
+    sql"""UPDATE products
+         |SET name = ${product.name}, stock = ${product.stock}
+         |WHERE id = ${product.id}""".stripMargin.update
+
+  private def deleted(id: String): Update0 =
+    sql"DELETE FROM products WHERE id = $id".update
 }
