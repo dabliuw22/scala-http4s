160e6867aad40051b58d7c1937e84155d053759c
Will Leyton <wleylop22@gmail.com>
2021-01-29 08:40:55 -0500

    Update Spec

--- src/test/scala/com/leysoft/products/Spec.scala
+++ src/test/scala/com/leysoft/products/Spec.scala
@@ -1,12 +1,7 @@
 package com.leysoft.products
 
-import org.scalamock.scalatest.MockFactory
 import org.scalatest.BeforeAndAfterEach
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
-trait Spec
-    extends AnyWordSpec
-    with Matchers
-    with MockFactory
-    with BeforeAndAfterEach {}
+trait Spec extends AnyWordSpec with Matchers with BeforeAndAfterEach {}
