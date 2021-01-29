5afcea8c17b2e2c3e8e560281459ac7371f77ded
Will Leyton <wleylop22@gmail.com>
2021-01-29 08:40:37 -0500

    Update ProductService

--- src/main/scala/com/leysoft/products/application/ProductService.scala
+++ src/main/scala/com/leysoft/products/application/ProductService.scala
@@ -1,7 +1,7 @@
 package com.leysoft.products.application
 
 import cats.effect.Effect
-import com.leysoft.products.domain.error.{MonadThrow, ProductNotFoundException, ProductWritingException}
+import com.leysoft.products.domain.error._
 import com.leysoft.products.domain.{Product, ProductRepository}
 import fs2.Stream
 
@@ -23,20 +23,23 @@ sealed trait ProductService[P[_]] {
 final class DefaultProductService[P[_]: Effect: MonadThrow](
   productRepository: ProductRepository[P]
 ) extends ProductService[P] {
-  import cats.syntax.monadError._
+  import cats.syntax.flatMap._
   import cats.syntax.functor._
+  import cats.syntax.monadError._
   import cats.syntax.applicativeError._
 
   override def get(id: String): P[Product] =
     productRepository
       .findBy(id)
-      .map {
-        case Some(value) => value
+      .flatMap {
+        case Some(value) => Effect[P].pure(value)
         case _ =>
-          throw ProductNotFoundException(s"Not Found Product By Id: $id")
+          Effect[P].raiseError[Product](
+            ProductNotFoundException(s"Not Found Product By Id: $id")
+          )
       }
-      .handleError(e =>
-        throw ProductNotFoundException(s"Not Found Product By Id: $id")
+      .handleErrorWith(e =>
+        Effect[P].raiseError(ProductNotFoundException(e.getMessage))
       )
 
   override def getAll: P[List[Product]] =
@@ -50,7 +53,7 @@ final class DefaultProductService[P[_]: Effect: MonadThrow](
   override def create(product: Product): P[Product] =
     productRepository
       .save(product)
-      .map {
+      .flatMap {
         validate(
           _,
           s"It Was Not Possible To Create The Product With id: ${product.id}",
@@ -62,7 +65,7 @@ final class DefaultProductService[P[_]: Effect: MonadThrow](
   override def update(product: Product): P[Product] =
     productRepository
       .update(product)
-      .map {
+      .flatMap {
         validate(
           _,
           s"It Was Not Possible To Update The Product With id: ${product.id}",
@@ -80,10 +83,10 @@ final class DefaultProductService[P[_]: Effect: MonadThrow](
       }
       .adaptError(e => ProductWritingException(e.getMessage))
 
-  private def validate[A](count: Int, message: String, a: A): A =
+  private def validate[A](count: Int, message: String, a: A): P[A] =
     count match {
-      case 0 => throw ProductWritingException(message)
-      case _ => a
+      case 0 => Effect[P].raiseError[A](ProductWritingException(message))
+      case _ => Effect[P].pure(a)
     }
 }
 
